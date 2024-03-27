2e5edb3a846ae56c7c1b738b95a8831c25f855de
Mdsaqiboffical <154467672+Mdsaqiboffical@users.noreply.github.com>
2024-03-27 20:07:46 +0500

    done

---
+++ app.js
@@ -0,0 +1,9 @@
+let newNumber = 0;
+function alertFunction() {
+    newNumber++;
+    alert(newNumber);
+}
+document.addEventListener("keydown", alertFunction);
+window.onload = function () {
+    newNumber = 0;
+}
